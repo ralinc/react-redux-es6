1545ec7e9ef68951c8ad044748551357d38bba87
Ralin Chimev <ralin.chimev@gmail.com>
2016-12-31 12:31:06 +0200

    Create CourseList component

---
+++ src/components/course/CourseListRow.js
@@ -0,0 +1,18 @@
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
+
+const CourseListRow = ({course}) => {
+  return (
+    <tr>
+      <td><Link to={'/course/' + course.id}>{course.title}</Link></td>
+      <td>{course.authorId}</td>
+      <td>{course.category}</td>
+    </tr>
+  );
+};
+
+CourseListRow.propTypes = {
+  course: PropTypes.object.isRequired
+};
+
+export default CourseListRow;
