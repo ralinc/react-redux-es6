a8ead2fbcab065cf5e3ac9753234077044e3a1bd
Ralin Chimev <ralin.chimev@gmail.com>
2017-01-01 13:12:33 +0200

    Create save course thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -19,3 +19,31 @@ export function loadCoursesSuccess(courses) {
     courses
   };
 }
+
+export function saveCourse(course) {
+  return function (dispatch) {
+    return courseApi.saveCourse(course).
+      then(saved => {
+        course.id ?
+          dispatch(updateCourseSuccess(saved)) :
+          dispatch(createCourseSuccess(saved));
+      }).
+      catch(error => {
+        console.log(error);
+      });
+  };
+}
+
+export function createCourseSuccess(course) {
+  return {
+    type: types.CREATE_COURSE_SUCCESS,
+    course
+  };
+}
+
+export function updateCourseSuccess(course) {
+  return {
+    type: types.UPDATE_COURSE_SUCCESS,
+    course
+  };
+}
