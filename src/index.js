a1fb285eb2e452eca7ecefe711f730add100eeb8
Ralin Chimev <ralin.chimev@gmail.com>
2017-01-01 12:48:18 +0200

    Create author reducer

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import { loadAuthors } from './actions/authorActions';
 import { loadCourses } from './actions/courseActions';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css';
 
 const store = configureStore();
+store.dispatch(loadAuthors());
 store.dispatch(loadCourses());
 
 render(
