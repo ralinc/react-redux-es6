c9eaddc164ff32e48e9d9c3696757429f23e87d2
Ralin Chimev <ralin.chimev@gmail.com>
2017-01-01 12:56:47 +0200

    Map authors for dropdown

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -17,7 +17,7 @@ class ManageCoursePage extends React.Component {
   render() {
     return (
       <CourseForm
-        authors={[]}
+        authors={this.props.authors}
         course={this.state.course}
         errors={this.state.errors}
       />
@@ -26,7 +26,8 @@ class ManageCoursePage extends React.Component {
 }
 
 ManageCoursePage.propTypes = {
-  course: PropTypes.object.isRequired
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state) {
@@ -37,8 +38,16 @@ function mapStateToProps(state) {
     category: ''
   };
 
+  const authors = state.authors.map(author => {
+    return {
+      value: author.id,
+      text: author.firstName + ' ' + author.lastName
+    };
+  });
+
   return {
-    course: course
+    course: course,
+    authors: authors
   };
 }
 
