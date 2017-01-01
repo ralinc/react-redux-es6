4a7aa2753f95a6ee528145887df84925348743a1
Ralin Chimev <ralin.chimev@gmail.com>
2017-01-01 11:52:34 +0200

    Create manage course form

---
+++ src/components/course/CourseForm.js
@@ -0,0 +1,52 @@
+import React, {PropTypes} from 'react';
+import TextInput from '../common/TextInput';
+import SelectInput from '../common/SelectInput';
+
+const CourseForm = ({course, authors, onSave, onChange, loading, errors}) => {
+  return (
+    <form>
+      <h1>Manage Course</h1>
+      <TextInput
+        name="title"
+        label="Title"
+        value={course.title}
+        onChange={onChange}
+        error={errors.title}
+      />
+      <SelectInput
+        name="author"
+        label="Author"
+        value={course.authorId}
+        defaultOption="Select Author"
+        options={authors}
+        onChange={onChange}
+        errors={errors.authorId}
+      />
+      <TextInput
+        name="category"
+        label="Category"
+        value={course.category}
+        onChange={onChange}
+        error={errors.category}
+      />
+      <input
+        type="submit"
+        disabled={loading}
+        value={loading ? 'Saving...' : 'Save'}
+        className="btn btn-primary"
+        onClick={onSave}
+      />
+    </form>
+  );
+}
+
+CourseForm.propTypes = {
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired,
+  loading: PropTypes.bool,
+  errors: PropTypes.object
+};
+
+export default CourseForm;
